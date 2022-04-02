5b6261c694117baf2a6c01fa19765969bdc2dcc6
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2022-04-02 19:26:23 +0300

    Allow CORS

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -4,56 +4,62 @@ const bcrypt = require("bcrypt");
 const User = db.users;
 
 exports.login = (req, res) => {
+    try {
+        User.findOne({
+            email: req.body.email
+        })
+            .exec((err, user) => {
+                if (err) {
+                    return res.status(500)
+                        .send({
+                            message: err
+                        });
+                }
+                if (!user) {
+                    return res.status(404)
+                        .send({
+                            message: "User Not found."
+                        });
+                }
 
-    User.findOne({
-        email: req.body.email
-    })
-        .exec((err, user) => {
-            if (err) {
-                return res.status(500)
-                    .send({
-                        message: err
-                    });
-            }
-            if (!user) {
-                return res.status(404)
-                    .send({
-                        message: "User Not found."
-                    });
-            }
-
-            //comparing passwords
-            const passwordIsValid = bcrypt.compareSync(
-                req.body.password,
-                user.password
-            );
-            // checking if password was valid and send response accordingly
-            if (!passwordIsValid) {
-                return res.status(401)
-                    .send({
-                        accessToken: null,
-                        message: "Invalid Password!"
-                    });
-            }
-            //signing token with user id
-            const token = jwt.sign({
-                id: user.id
-            }, process.env.API_SECRET, {
-                expiresIn: 86400
-            });
-
-            //responding to client request with user profile success message and  access token .
-            return res.status(200)
-                .send({
-                    user: {
-                        id: user._id,
-                        email: user.email,
-                        fullName: user.fullName,
-                    },
-                    message: "Login successfull",
-                    accessToken: token,
+                //comparing passwords
+                const passwordIsValid = bcrypt.compareSync(
+                    req.body.password,
+                    user.password
+                );
+                // checking if password was valid and send response accordingly
+                if (!passwordIsValid) {
+                    return res.status(401)
+                        .send({
+                            accessToken: null,
+                            message: "Invalid Password!"
+                        });
+                }
+                //signing token with user id
+                const token = jwt.sign({
+                    id: user.id
+                }, process.env.API_SECRET, {
+                    expiresIn: 86400
                 });
-        });
+
+                //responding to client request with user profile success message and  access token .
+                return res.status(200)
+                    .send({
+                        user: {
+                            id: user._id,
+                            email: user.email,
+                            fullName: user.fullName,
+                        },
+                        message: "Login successfull",
+                        accessToken: token,
+                    });
+            });
+    } catch (e) {
+        return res.status(500)
+            .send({
+                e
+            });
+    }
 };
 
 exports.register = (req, res) => {
