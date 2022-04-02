5a9adfb1849e41faebc7ec224b64d86b8d74d73a
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2022-04-02 18:08:03 +0300

    Allow CORS

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -5,20 +5,6 @@ const User = db.users;
 
 exports.login = (req, res) => {
 
-    // res.setHeader('Access-Control-Allow-Credentials', true)
-    // res.setHeader('Access-Control-Allow-Origin', '*')
-    // // another common pattern
-    // // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    // res.setHeader(
-    //     'Access-Control-Allow-Headers',
-    //     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    // )
-    // if (req.method === 'OPTIONS') {
-    //     res.status(200).end()
-    //     return
-    // }
-
     User.findOne({
         email: req.body.email
     })
