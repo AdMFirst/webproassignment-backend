f39cd61cb86ac2f963f3647626f15dec3fe6f84a
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2022-04-02 18:18:27 +0300

    Allow CORS

--- index.js
+++ index.js
@@ -47,9 +47,7 @@ require("./app/routes/auth.routes")(app, cors, corsOptions);
 require("./app/routes/post.routes")(app);
 require("./app/routes/image.routes")(app, upload);
 
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
+app.use(cors());
 
 
 // set port, listen for requests
