6bc3a9bba48fedc925b3fa5bfa22fe7758691349
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2022-04-02 14:20:24 +0300

    Allow CORS

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,12 +1,11 @@
 module.exports = (app, cors, corsOptions) => {
     const auth = require("../controllers/auth.controller.js");
-    const allowCors = require("../middlewares/allowCors");
 
     let router = require("express").Router();
 
     router.post("/register", auth.register);
 
-    router.post("/login", allowCors, auth.login);
+    router.post("/login", auth.login);
 
     app.use("/auth", router);
 };
