8b0f044c7befb1ec74b81f0c1acaeac503d34839
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2022-04-02 20:10:39 +0300

    Minor fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const multer = require("multer");
 const cors = require('cors')
 
-const whitelist = ['https://www.eurocarpathian.com', 'http://locslhost:3000']
+const whitelist = ['https://www.eurocarpathian.com', 'http://localhost:3000']
 const corsOptions = {
     origin: function (origin, callback) {
         console.log(origin)
@@ -25,14 +25,6 @@ app.use(express.json({extended: false}));  /* bodyParser.json() is deprecated */
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({extended: true}));   /* bodyParser.urlencoded() is deprecated */
-
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", '*');
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header("Access-Control-Allow-Headers", 'Access-Control-Request-Headers,Access-Control-Request-Method,Authorization,authorization,Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//     next();
-// });
 
 const storage = multer.memoryStorage()
 const upload = multer({storage: storage})
@@ -56,16 +48,12 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to eurocarpathian API."});
 });
 
-// const auth = require("./app/controllers/auth.controller");
-
-// app.post("/auth/login", auth.login);
 
 require("./app/routes/auth.routes")(app, cors, corsOptions);
 require("./app/routes/post.routes")(app, cors, corsOptions);
 require("./app/routes/image.routes")(app, upload, cors, corsOptions);
 
 app.use(cors());
-
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
