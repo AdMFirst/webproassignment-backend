8096387aecb6dbf5ca5edaf1f4720816d12fc969
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2022-04-11 16:40:53 +0300

    Change Image Model

--- app/controllers/post.controller.js
+++ app/controllers/post.controller.js
@@ -4,202 +4,198 @@ const Image = db.images;
 
 // Create and Save a new Post
 exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.title) {
-        res.status(400).send({message: "Content can not be empty!"});
-        return;
-    }
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
 
-    // Create a Post
-    const post = new Post({
-        title: req.body.title,
-        description: req.body.description,
-        titleEN: req.body.titleEN,
-        descriptionEN: req.body.descriptionEN,
-        published: req.body.published || false,
-        created: req.body.created,
-        imageId: req.body.imageId,
-        videoId: req.body.videoId,
-        userId: req.user.id
+  // Create a Post
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+    titleEN: req.body.titleEN,
+    descriptionEN: req.body.descriptionEN,
+    published: req.body.published || false,
+    created: req.body.created,
+    imageId: req.body.imageId,
+    videoId: req.body.videoId,
+    userId: req.user.id
+  });
+
+  // Save Post in the database
+  post
+    .save(post)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Post."
+      });
     });
-
-    // Save Post in the database
-    post
-        .save(post)
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the Post."
-            });
-        });
 };
 
 // Retrieve all Posts from the database.
 exports.findAll = (req, res) => {
-    const title = req.query.title;
-    const condition = title ? {title: {$regex: new RegExp(title), $options: "i"}} : {};
+  const title = req.query.title;
+  const condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
 
-    Post.find(condition)
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving posts."
-            });
-        });
+  Post.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving posts."
+      });
+    });
 };
 
 // Find a single Post with an id
 exports.findOne = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Post.findById(id)
-        .then(data => {
-            if (!data)
-                res.status(404).send({message: "Not found Post with id " + id});
-            else res.send(data);
-        })
-        .catch(err => {
-            res
-                .status(500)
-                .send({message: "Error retrieving Post with id=" + id});
-        });
+  Post.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Post with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Post with id=" + id });
+    });
 };
 
 // Update a Post by the id in the request
 exports.update = (req, res) => {
-    if (!req.body) {
-        return res.status(400).send({
-            message: "Data to update can not be empty!"
-        });
-    }
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
 
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Post.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
-        .then(data => {
-            if (!data) {
-                res.status(404).send({
-                    message: `Cannot update Post with id=${id}. Maybe Post was not found!`
-                });
-            } else {
-                Image.updateOne(
-                    {_id: req.body.imageId},
-                    {postId: id},
-                    {multi: true},
-                    function (err, numberAffected) {
-                        if (err) {
-                            res.status(500).send({
-                                err
-                            });
-                        }
-                        res.send({message: "Post was updated successfully.", numberAffected});
-                    });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error updating Post with id=" + id
-            });
+  Post.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Post with id=${id}. Maybe Post was not found!`
         });
+      } else {
+        Image.findByIdAndUpdate(req.body.imageId, { postId: id }, { useFindAndModify: false }).then(data => {
+          res.send({message: "Post was updated successfully."});
+        }).catch(err => {
+          res.status(500).send({
+            message: "Error updating Post with id=" + id
+          });
+        });
+
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Post with id=" + id
+      });
+    });
 };
 
 // Delete a Post with the specified id in the request
 exports.delete = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Post.findByIdAndRemove(id, {useFindAndModify: false})
-        .then(data => {
-            if (!data) {
-                res.status(404).send({
-                    message: `Cannot delete Post with id=${id}. Maybe Post was not found!`
-                });
-            } else {
-                res.send({
-                    message: "Post was deleted successfully!"
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Could not delete Post with id=" + id
-            });
+  Post.findByIdAndRemove(id, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Post with id=${id}. Maybe Post was not found!`
         });
+      } else {
+        res.send({
+          message: "Post was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Post with id=" + id
+      });
+    });
 };
 
 // Delete all Posts from the database.
 exports.deleteAll = (req, res) => {
-    Post.deleteMany({})
-        .then(data => {
-            res.send({
-                message: `${data.deletedCount} Post were deleted successfully!`
-            });
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while removing all posts."
-            });
-        });
+  Post.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Post were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all posts."
+      });
+    });
 };
 
 // Find all published Posts
 exports.findAllPublished = (req, res) => {
-    Post.find({published: true}).sort({created: -1})
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving posts."
-            });
-        });
+  Post.find({ published: true }).sort({created: -1})
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving posts."
+      });
+    });
 };
 
 // Find all published Posts
 exports.translate = async (req, res) => {
-    if (!req.body.text) {
-        res.status(400).send({message: "Content can not be empty!"});
-        return;
-    }
+  if (!req.body.text) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
 
-    try {
-        const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-        const {IamAuthenticator} = require('ibm-watson/auth');
+  try {
+    const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
+    const { IamAuthenticator } = require('ibm-watson/auth');
 
-        const languageTranslator = new LanguageTranslatorV3({
-            version: process.env.TRANSLATOR_VERSION,
-            authenticator: new IamAuthenticator({
-                apikey: process.env.TRANSLATOR_API_KEY,
-            }),
-            serviceUrl: process.env.TRANSLATOR_URL,
-            disableSslVerification: true,
+    const languageTranslator = new LanguageTranslatorV3({
+      version: process.env.TRANSLATOR_VERSION,
+      authenticator: new IamAuthenticator({
+        apikey: process.env.TRANSLATOR_API_KEY,
+      }),
+      serviceUrl: process.env.TRANSLATOR_URL,
+      disableSslVerification: true,
+    });
+
+    const translateParams = {
+      modelId: 'uk-en',
+      text: req.body.text,
+    };
+
+    languageTranslator.translate(translateParams)
+        .then(data => {
+          res.status(200).send(data.result.translations.map(t => t.translation));
+        })
+        .catch(err => {
+          console.log('error:', err);
+          res.status(500).send({
+            message: err
+          });
         });
-
-        const translateParams = {
-            modelId: 'uk-en',
-            text: req.body.text,
-        };
-
-        languageTranslator.translate(translateParams)
-            .then(data => {
-                res.status(200).send(data.result.translations.map(t => t.translation));
-            })
-            .catch(err => {
-                console.log('error:', err);
-                res.status(500).send({
-                    message: err
-                });
-            });
-    } catch (e) {
-        res.status(500).send({
-            message: e
-        });
-    }
+  } catch (e) {
+    res.status(500).send({
+      message: e
+    });
+  }
 };
