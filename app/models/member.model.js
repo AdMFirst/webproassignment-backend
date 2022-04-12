9f6d1718ad97a1fc8ca7d2b16c1c3cfea4d1f3b6
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2022-04-12 20:08:17 +0300

    Change Member Model

--- app/models/member.model.js
+++ app/models/member.model.js
@@ -11,7 +11,6 @@ module.exports = mongoose => {
             },
             description: {
                 type: String,
-                required: [true, "Description not provided"],
             },
             en: {
                 fullName: {
@@ -24,7 +23,6 @@ module.exports = mongoose => {
                 },
                 description: {
                     type: String,
-                    required: [true, "Description EN not provided"],
                 },
             },
             published: Boolean,
