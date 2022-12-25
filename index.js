ef2bb99ef4e7369a071b726a086b97acfb6f0d90
Aditya MP <211wadityaw112@gmail.com>
2022-12-25 20:49:43 +0700

    follow cyclic guide

--- index.js
+++ index.js
@@ -30,8 +30,18 @@ app.use(express.urlencoded({extended: true}));   /* bodyParser.urlencoded() is d
 
 // removed multer cause some vulnerability
 
+app.get("/", (req, res) => {
+    res.json({message: "Welcome to Swift E-Learning API."});
+});
 
 const db = require("./app/models/schema");
+require("./app/routes/auth.routes")(app, cors, corsOptions);
+require("./app/routes/protected.routes")(app, cors, corsOptions);
+
+app.use(cors(corsOptions));
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
 db.mongoose
     .connect(db.url, {
         useNewUrlParser: true,
@@ -41,25 +51,14 @@ db.mongoose
     })
     .then(() => {
         console.log("Connected to the database!");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}.`);
+        });
     })
     .catch(err => {
         console.log("Cannot connect to the database!", err);
         process.exit();
     });
 
-app.get("/", (req, res) => {
-    res.json({message: "Welcome to Swift E-Learning API."});
-});
 
 
-require("./app/routes/auth.routes")(app, cors, corsOptions);
-require("./app/routes/protected.routes")(app, cors, corsOptions);
-
-
-app.use(cors(corsOptions));
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
